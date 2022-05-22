d8f5a18a0b8c83d3fdb8f235f2d0416228d9cb98
juandavid7777 <juandavid7777@gmail.com>
2022-05-22 20:13:45 +0400

    packages

--- BTC_projection.py
+++ BTC_projection.py
@@ -7,7 +7,7 @@ import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
-from datetime import datetime
+#from datetime import datetime
 
 
 #1.-----Downloads data
