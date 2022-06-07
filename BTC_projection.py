f3e3369b89149dcff9380c3326d8789c5f782fbb
juandavid7777 <juandavid7777@gmail.com>
2022-06-07 09:22:46 +0400

    Update BTC_projection.py

--- BTC_projection.py
+++ BTC_projection.py
@@ -92,7 +92,7 @@ acc_HOLD_gains = (risk_adj_price*BTCout-last_price*BTCin)/(last_price*BTCin)*100
 strl.image("bitcoin.jpg")
 
     #Title
-strl.markdown('<b style="color:darkgray ; font-size: 44px">Bitcoin logarithmic investment projections</b>', unsafe_allow_html=True)
+strl.markdown('<b style="color:darkgoldenrod ; font-size: 44px">Bitcoin logarithmic investment projections</b>', unsafe_allow_html=True)
 
 
     #Creates triple column
